4691821046371a8c0f0ff7d7c649cf8cdf6e110d
Jiayu Ye <106568221+yjy-97@users.noreply.github.com>
2025-11-13 15:10:02 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 
 from my_dataset import MyDataSet
-from model.MFor import MADFormer
+from model.MFor import MADFormerModel
 from utils import read_split_data, create_lr_scheduler, get_params_groups, train_one_epoch, evaluate
 
 import warnings
